c676e94cc0038501caf4eb479b21de3e1fa849cc
Aim-win <medamin.mahdam@gmail.com>
2025-05-14 15:50:48 +0100

    changing from nodejs to php

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/auth.js
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Route d'inscription
-router.post('/register', authController.registerUser);
-
-module.exports = router;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: './',
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost/php-backend',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+});
 
