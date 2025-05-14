b9ae6d069ff953b5c604c8433261d89dd08eef8a
Aim-win <medamin.mahdam@gmail.com>
2025-05-14 16:26:59 +0100

    reset frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost/php-backend',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-});
-
+  plugins: [react()],
+  base: './',
+})
